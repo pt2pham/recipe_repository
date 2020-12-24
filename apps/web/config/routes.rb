4e5d790f1606cd143ca64dc6da4d56a84c4c224d
Phong Pham <phongpham1342@hotmail.com>
2020-12-24 18:24:37 -0500

    Add show action and route

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -5,4 +5,4 @@
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 root to: 'home#index'
 
-resources :recipes, only: [:index, :new, :create]
+resources :recipes, only: [:index, :new, :create, :show]
