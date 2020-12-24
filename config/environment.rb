c63ea97ba12ab08fc2420daa4f2abdf99fcc0612
Phong Pham <phongpham1342@hotmail.com>
2020-12-24 12:17:33 -0500

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/recipe_repository'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/recipe_repository_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/recipe_repository_development'
+    #    adapter :sql, 'mysql://localhost/recipe_repository_development'
+    #
+    adapter :sql, ENV.fetch('DATABASE_URL')
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/recipe_repository/mailers'
+
+    # See https://guides.hanamirb.org/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: https://guides.hanamirb.org/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV.fetch('SMTP_HOST'), port: ENV.fetch('SMTP_PORT')
+    end
+  end
+end
---
+++ lib/recipe_repository.rb
@@ -0,0 +1,2 @@
+module RecipeRepository
+end
