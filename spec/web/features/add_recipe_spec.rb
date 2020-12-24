16c07bd6c071c0ee36a7426607176efd03416917
Phong Pham <phongpham1342@hotmail.com>
2020-12-24 16:50:28 -0500

    [fix] Fixed problem with Add Recipe spec

--- spec/web/features/add_recipe_spec.rb
+++ spec/web/features/add_recipe_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe 'Add a recipe' do
 
     within 'form#recipe-form' do
       fill_in 'Name',  with: 'Name of your recipe'
-      fill_in 'Difficulty', with: 'A difficulty rating from 0-5'
-      fill_in 'Food Type', with: 'The type of dish'
-      fill_in 'Serving Size', with: 'Number of people'
-      fill_in 'Duration', with: 'Cooking/prep time'
+      fill_in 'Difficulty', with: 0.0
+      fill_in 'Food type', with: 'The type of dish'
+      fill_in 'Serving size', with: 'Number of people'
+      fill_in 'Duration', with: 0
       fill_in 'Ingredients', with: 'List of ingredients'
       fill_in 'Instructions', with: 'Numbered instructions'
       fill_in 'Notes', with: 'Any special notes to be aware of'
@@ -24,6 +24,6 @@ RSpec.describe 'Add a recipe' do
     end
 
     expect(page).to have_current_path('/recipes')
-    expect(page).to have_content('Example recipe')
+    expect(page).to have_content('Name of your recipe')
   end
 end
